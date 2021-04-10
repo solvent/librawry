5aca38d20f92ab7e7be9a748a5e9530f017193ac
Maksim Gurianov <maksim.solvent@gmail.com>
2021-04-10 14:19:03 +0300

    clean up usings

--- Pages/Search.cshtml.cs
+++ Pages/Search.cshtml.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using librawry.classes;
 using librawry.classes.entities;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
--- classes/entities/TagRef.cs
+++ classes/entities/TagRef.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace librawry.classes.entities {
 
 	public class TagRef {
