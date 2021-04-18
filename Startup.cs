7f57ea2a0e5f7fbf667d1a3f102d4f355543d733
Maksim Gurianov <maksim.solvent@gmail.com>
2021-04-18 15:21:51 +0300

    migrated to netcore3.1

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,6 @@
 using librawry.classes;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,18 +14,21 @@ namespace librawry {
 		public IConfiguration Configuration { get; }
 
 		public void ConfigureServices(IServiceCollection services) {
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			services.AddRazorPages();
 
 			var cons = Configuration.GetConnectionString("SqliteDatabase");
 			services.AddDbContext<SqliteContext>(options => options.UseSqlite(cons));
 		}
 
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 			app.UseDeveloperExceptionPage();
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
+			app.UseRouting();
 
-			app.UseMvc();
+			app.UseEndpoints(endpoints => {
+				endpoints.MapRazorPages();
+			});
 		}
 	}
 }
